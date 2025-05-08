8e6ba95b757bf5986846385b6fbb1e3daad6ccd8
IanMV <ianmatheusv54@gmail.com>
2025-05-08 09:02:08 -0300

    FEAT: Adicionando cabeçalho e rodapé da página

---
+++ frontend/components/highcomponents/MyFooter.vue
@@ -0,0 +1,111 @@
+<template>
+  <footer>
+    <div>
+      <ul id="nav">
+        <li id="medias">
+          <p>IFbooks</p>
+          <ul>
+            <li><span class="fa-brands fa-square-facebook"></span></li>
+            <li><span class="fa-brands fa-square-instagram"></span></li>
+            <li><span class="fa-brands fa-square-twitter"></span></li>
+          </ul>
+        </li>
+        <li id="contact">
+          <p>Contato</p>
+          <ul>
+            <li><span class="fa-solid fa-phone"></span>+55 47 40045263</li>
+            <li>
+              <span class="fa-solid fa-clock"></span>8h às 23h - Seg a Sex
+            </li>
+            <li>
+              <span class="fa-solid fa-envelope"></span>contato@ifbooks.com
+            </li>
+          </ul>
+          <ul>
+            <li><img src="../../src/paymentmethods/paipalcard.png" alt="" /></li>
+            <li><img src="../../src/paymentmethods/mastercardcard.png" alt="" /></li>
+            <li><img src="../../src/paymentmethods/visacard.png" alt="" /></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <p id="direitos-reservados">© Alguns direitos reservados. IFbooks 2025.</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background-color: var(--verde-claro);
+  color: var(--branco);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div {
+    width: 100%;
+    border-bottom: 1px solid var(--branco);
+
+    #nav {
+      padding: calc(20px + 1vw) calc(20px + 4vw);
+      display: flex;
+      justify-content: space-between;
+      li {
+        ul {
+          display: flex;
+        }
+        &#contact {
+          display: flex;
+          flex-direction: column;
+          align-items: baseline;
+          justify-content: center;
+          gap: calc(20px + 0.6vh);
+
+          ul:nth-child(2) {
+            gap: calc(8px + 0.5vh);
+            flex-direction: column;
+            color: var(--branco);
+
+            li {
+              opacity: 0.6;
+              span {
+                font-size: calc(13px + 0.3vw);
+                margin-right: calc(3px + 0.3vw);
+              }
+            }
+          }
+          ul:last-child {
+            gap: calc(10px + 1vw);
+            align-items: center;
+            justify-content: baseline;
+            img {
+              width: calc(40px + 0.8vw);
+            }
+          }
+        }
+        &#medias {
+          ul {
+            margin-top: calc(20px + 0.6vh);
+            gap: calc(12px + 0.3vw);
+            li {
+              span {
+                font-size: calc(25px + 0.6vw);
+              }
+            }
+          }
+        }
+      }
+
+      @media (max-width: 500px) {
+        &{
+          padding: calc(20px + 1vw) calc(13px + 2vw); 
+        }
+      }
+
+    }
+  }
+  #direitos-reservados {
+    color: var(--branco);
+    padding: calc(25px + 1.5vh) calc(30px + 3vw);
+    opacity: 0.6;
+  }
+}
+</style>
---
+++ frontend/components/highcomponents/MyHeader.vue
@@ -0,0 +1,258 @@
+<script lang="ts" setup>
+import SearchCamp from "../smallcomponents/SearchCamp.vue";
+import { SearchStore } from "../../store/SearchStore";
+const search = SearchStore();
+
+import { UserStore } from "../../store/UserStore";
+const user = UserStore();
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul class="nav">
+        <li class="nav-item">
+          <ul class="nav-item-logo">
+            <li><router-link to="/">IFbooks</router-link></li>
+            <hr />
+            <li style="opacity: 0.6" class="greentext">
+              Apreço a<br />
+              leitura
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <div class="nav-item_search">
+            <div>
+              <input
+                type="text"
+                placeholder="Pesquisar"
+                v-model="search.search"
+              /><span class="fa-solid fa-magnifying-glass"></span>
+            </div>
+            <div>
+              <SearchCamp />
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <ul class="nav-item-nav">
+            <li><router-link to="/">Termos</router-link></li>
+            <li><router-link to="/">Equipe</router-link></li>
+            <li><router-link to="/">Envio</router-link></li>
+            <li><router-link to="/">Devoluções</router-link></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <ul class="nav-item-user">
+            <li class="nav-item-user-name greentext" v-if="user.user?.name">
+              <router-link to="/login"
+                ><span class="fa-solid fa-user greentext"></span
+                >{{ user.user?.name }}</router-link
+              >
+            </li>
+            <li class="nav-item-user-login" v-else>
+              <router-link to="/login">Login</router-link>
+            </li>
+            <hr />
+            <li class="greentext">
+              <router-link to="/"
+                ><span class="fa-solid fa-heart"></span
+              ></router-link>
+            </li>
+            <hr />
+            <li class="greentext">
+              <router-link to="/cart"
+                ><span class="fa-solid fa-cart-shopping"></span
+              ></router-link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+hr {
+  height: calc(10px + 4vh);
+  width: 1px;
+  background-color: var(--verde-claro);
+  border: none;
+}
+header {
+  font-size: var(--texto);
+  border-bottom: 1px solid var(--verde-claro);
+  nav {
+    margin: calc(10px + 1vw) calc(20px + 4vw);
+    .nav {
+      display: grid;
+      grid-template-columns: 1fr 2fr 1.8fr 0.8fr;
+      align-items: center;
+      gap: calc(8px + 2vw);
+      grid-auto-rows: 1fr 1fr;
+
+      @media (max-width: 1024px) {
+        & {
+          grid-template-columns: 1fr 1fr;
+          grid-template-rows: 1fr 1fr;
+          .nav-item {
+            &:nth-child(1) {
+              order: 1;
+            }
+            &:nth-child(2) {
+              order: 3;
+            }
+            &:nth-child(3) {
+              order: 2;
+            }
+            &:nth-child(4) {
+              order: 4;
+            }
+          }
+        }
+      }
+      @media (max-width: 600px) {
+        & {
+          grid-template-columns: 1fr 1fr;
+          grid-template-rows: 1fr 1fr 1fr;
+          .nav-item {
+            &:nth-child(1) {
+              order: 2;
+            }
+            &:nth-child(2) {
+              order: 4;
+            }
+            &:nth-child(3) {
+              order: 1;
+              grid-column: 1/3;
+              .nav-item-nav {
+                margin-right: calc(2px + 4vw);
+                li {
+                  width: calc(10% + 9vw);
+                }
+              }
+            }
+            &:nth-child(4) {
+              order: 3;
+            }
+          }
+        }
+      }
+
+      .nav-item {
+        .nav-item-logo {
+          display: flex;
+          align-items: center;
+          justify-content: baseline;
+          gap: calc(10px + 0.3vw);
+
+          hr {
+            height: calc(17px + 4vh);
+          }
+
+          li {
+            a {
+              text-decoration: none;
+              color: var(--preto);
+            }
+          }
+        }
+        .nav-item-nav {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: calc(8px + 0.5vw);
+
+          li {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            a {
+              height: 100%;
+              width: 100%;
+              text-align: center;
+              text-decoration: none;
+              color: rgb(141, 141, 141);
+            }
+          }
+        }
+
+        .nav-item-user {
+          display: flex;
+          align-items: center;
+          justify-content: end;
+          gap: calc(5px + 1vw);
+
+          li {
+            a {
+              color: var(--verde-claro);
+            }
+          }
+
+          .nav-item-user-login {
+            a {
+              text-align: center;
+              text-decoration: none;
+              color: var(--verde-claro);
+              border: 1px solid var(--verde-claro);
+              padding: calc(2px + 0.2vh) calc(3px + 0.5vw);
+            }
+          }
+          .nav-item-user-name {
+            a {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+              gap: calc(1px + 0.2vw);
+              text-decoration: none;
+            }
+          }
+        }
+        .nav-item_search {
+          display: block;
+
+          div {
+            display: flex;
+            justify-content: baseline;
+            align-items: center;
+
+            span {
+              font-size: 15px;
+              padding: 10px;
+              background-color: rgba(241, 241, 241, 1);
+            }
+            input {
+              background-color: rgba(241, 241, 241, 1);
+              border-radius: 3px;
+              outline: none;
+              border: none;
+              height: 35px;
+              max-width: 287.25px;
+              width: calc(80px + 18vw);
+              padding-left: calc(3px + 0.8vw);
+            }
+
+            &:last-child {
+              position: relative;
+            }
+          }
+        }
+      }
+    }
+
+    @media (max-width: 500px) {
+      & {
+        margin: calc(20px + 1vw) calc(10px + 4vw);
+      }
+    }
+  }
+}
+.greentext {
+  color: var(--verde-claro);
+}
+</style>
