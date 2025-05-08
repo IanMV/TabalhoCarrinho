486e03376750183493ec85cfc697f54a0a71542c
IanMV <ianmatheusv54@gmail.com>
2025-05-08 09:00:18 -0300

    FEAT: Adicionando os componentes das páginas de cadastro e de login e o css das duas páginas

---
+++ frontend/components/smallcomponents/EmailCamp.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import { UserStore } from "../../store/UserStore";
+const user = UserStore();
+</script>
+<template>
+  <input type="text" v-model="user.email" placeholder="Email..." />
+</template>
